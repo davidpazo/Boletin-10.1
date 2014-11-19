6384eeaf4a5a13f7475e93ab760ae57533823392
dpazolopez <dpazolopez@eniac21i.danielcastelao.org>
2014-11-19 11:26:06 +0100

    finish

---
+++ src/Boletin101/Boletin101.java
@@ -0,0 +1,8 @@
+package Boletin101;
+public class Boletin101 {
+    public static void main(String[] args) {
+        Numeros objeto= new Numeros();
+        objeto.jugar();
+    }
+    
+}
---
+++ src/Boletin101/Numeros.java
@@ -0,0 +1,53 @@
+package Boletin101;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author dpazolopez
+ */
+public class Numeros {
+
+    private int n;
+
+    public int pedirNum() {
+
+        do {
+            String dato = JOptionPane.showInputDialog("Inserta un número entre 1 y 50");
+            n = Integer.parseInt(dato);
+
+        } while (n < 1 || n > 50);
+        return n;
+    }
+
+    public int Intentos() {
+        do {
+            String valor = JOptionPane.showInputDialog("Inserte numero de intentos");
+            n = Integer.parseInt(valor);
+        } while (n <= 0);
+        return n;
+    }
+
+    public double jugar() {
+        float resp = this.pedirNum();
+        double intento = this.Intentos();
+        float num;
+        do {
+            String dato = JOptionPane.showInputDialog("DALE AHÍ");
+            num = Float.parseFloat(dato);
+            n++;
+            if (num < resp) {
+                JOptionPane.showMessageDialog(null, "CALIENTEE");
+            }
+            if (num > resp) {
+                JOptionPane.showMessageDialog(null, "FRIIIIOO");
+            }
+        } while (num != resp && n != intento);
+        if (num == resp) {
+            JOptionPane.showMessageDialog(null, "WIN");
+        }
+        if (n == intento) {
+            JOptionPane.showMessageDialog(null, "LOSE");
+        }
+        return n;
+    }
+}
